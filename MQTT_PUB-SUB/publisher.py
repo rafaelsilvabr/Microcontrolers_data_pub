5e2c399bf0db5c1e0a67e194a4dafe02e62ff52e
Rafael Silva <r4faelrs@gmail.com>
2021-01-11 10:36:11 -0300

    upd msg

--- MQTT_PUB-SUB/publisher.py
+++ MQTT_PUB-SUB/publisher.py
@@ -10,8 +10,35 @@ client = mqtt.Client()
 client.username_pw_set("ctuqpqym","Xk5GNcWqcmZG")
 client.connect(MQTT_ADDRESS,MQTT_PORT,MQTT_TIMEOUT)
 
+'''
 msg={'temp': 20,
     'humid': 50}
+'''
+
+msg = {'estado':False,
+	'registred':False,
+	'localId':'id1',
+	'regInfos':{
+	    "data": {
+	      "description": "A simple sensor",
+	      "capabilities": [
+	        "n",
+	        "temperature",
+	        "humidity",
+	        "pressure"
+	      ],
+	      "status": "active",
+	      "lat":10,
+	      "lon":12
+	    }
+	},
+	'data':{
+		"data": [{
+	      "temperature": 30
+	    }]
+	}
+}
+
 
 #client.publish("test","helloworld",qos=1,retain=True)
-client.publish("test",json.dumps(msg),qos=1,retain=True)
+client.publish("data",json.dumps(msg),qos=1,retain=True)
